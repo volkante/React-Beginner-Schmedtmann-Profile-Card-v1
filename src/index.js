2348bff78f6debbbfc1e3efa1e904400d73a1865
volkantemelli <volkan.temelli@gmail.com>
2024-03-29 21:06:00 +0300

    Project completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="/seaside.jpg" alt="volkan temelli" />;
+}
+
+function Intro() {
+  return (
+    <>
+      <h1>Volkan Temelli</h1>
+      <p>
+        Front-end web developer. MA Media Culture. Outside of coding, catch me
+        zooming down bike paths, wandering off, or getting lost in the rabbit
+        hole of Wikipedia's wonders.
+      </p>
+    </>
+  );
+}
+
+function SkillList() {
+  return (
+    <ul className="skill-list">
+      <Skill text="HTML+CSS 💪" color="#2c51c9" />
+      <Skill text="Javascript 💪" color="#edce1a" />
+      <Skill text="React 💪" color="#4dd3f0" />
+      <Skill text="Git and GitHub &#x1F44D;" color="#e0472c" />
+    </ul>
+  );
+}
+
+function Skill(props) {
+  return (
+    <li className="skill" style={{ backgroundColor: props.color }}>
+      {props.text}
+    </li>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
